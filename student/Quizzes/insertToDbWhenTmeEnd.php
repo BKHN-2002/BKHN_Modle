4c3825cdd6968e47ac3ce3f6dbdfa505b557f485
BKHN-2002 <ghabayenhussej@gmail.com>
2022-05-09 23:26:58 +0300

    update

--- student/Quizzes/insertToDbWhenTmeEnd.php
+++ student/Quizzes/insertToDbWhenTmeEnd.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
-include_once "../modle/DB.php";
+include_once "../../modle/DB.php";
 $student=$_GET['id'];
 $id=$_GET['quizId'];
 $sql = "INSERT INTO `quizzesgrades`(`quizId`, `studentId`, `grade`) VALUES ('$id','$student',0)";
 $result = mysqli_query($connection, $sql);
 echo $student;
-header("Location:student.php?id=$student");
+header("Location:../student.php");
